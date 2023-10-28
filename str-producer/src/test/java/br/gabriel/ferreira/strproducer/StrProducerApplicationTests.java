6a52ca59c18c9d2efa6c737e1202419731a7b96b
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-28 19:33:09 -0300

    feat: estrutura inicial

---
+++ str-producer/src/test/java/br/gabriel/ferreira/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gabriel.ferreira.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
